cf019f5f2317ade9a740a57671de51e080e4a1d2
stefanusgusega <gunawanstefanus006@gmail.com>
2022-02-26 19:34:40 +0700

    feat: add convert audio to numpy and util to filter covid and cough

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -1,11 +1,12 @@
-import os
-import string
-import pandas as pd
+from typing import Tuple
 import numpy as np
+import pandas as pd
 import librosa
+import os
+import pickle as pkl
 
 
-def segment_cough(
+def generate_cough_mask(
     x, fs, cough_padding=0.2, min_cough_len=0.2, th_l_multiplier=0.1, th_h_multiplier=2
 ):
     """Preprocess the data by segmenting each file into individual coughs using a hysteresis comparator on the signal power
@@ -33,7 +34,7 @@ def segment_cough(
     seg_th_h = th_h_multiplier * rms
 
     # Segment coughs
-    coughSegments = []
+    cough_segments = []
     padding = round(fs * cough_padding)
     min_cough_samples = round(fs * min_cough_len)
     cough_start = 0
@@ -50,13 +51,13 @@ def segment_cough(
                     cough_end = i + padding if (i + padding < len(x)) else len(x) - 1
                     cough_in_progress = False
                     if cough_end + 1 - cough_start - 2 * padding > min_cough_samples:
-                        coughSegments.append(x[cough_start : cough_end + 1])
+                        cough_segments.append(x[cough_start : cough_end + 1])
                         cough_mask[cough_start : cough_end + 1] = True
             elif i == (len(x) - 1):
                 cough_end = i
                 cough_in_progress = False
                 if cough_end + 1 - cough_start - 2 * padding > min_cough_samples:
-                    coughSegments.append(x[cough_start : cough_end + 1])
+                    cough_segments.append(x[cough_start : cough_end + 1])
             else:
                 below_th_counter = 0
         else:
@@ -64,31 +65,45 @@ def segment_cough(
                 cough_start = i - padding if (i - padding >= 0) else 0
                 cough_in_progress = True
 
-    return coughSegments, cough_mask
+    return cough_segments, cough_mask
 
 
-def append_format(dataset_path: str, df: pd.DataFrame, formats: list, inplace=False):
-    ext_arr = []
+def convert_audio_to_numpy(
+    df: pd.DataFrame,
+    audio_folder_path: str,
+    path_dump=None,
+    sampling_rate: int = 16000,
+    filename_colname: str = "uuid",
+    ext_colname: str = "ext",
+    save_to_pickle: bool = True,
+) -> Tuple[np.ndarray, np.ndarray]:
+
+    samples = []
 
     for _, row in df.iterrows():
-        ext = ""
-        found = False
-        i = 0
-        while not found and i < len(formats):
-            if os.path.exists(os.path.join(dataset_path, row["uuid"] + formats[i])):
-                ext = formats[i]
-                found = True
+        filename = row[filename_colname] + row[ext_colname]
+        # Sampling rate is not returned because it will make worse memory usage
+        audio_data, _ = librosa.load(
+            os.path.join(audio_folder_path, filename), sr=sampling_rate
+        )
+        samples.append(audio_data)
 
-            i += 1
+    # Get the return for this function
+    res = np.array(samples).reshape(-1, 1), np.full((len(df), 1), sampling_rate)
 
-        if ext == "":
-            raise Exception("No audio file found.")
+    # Save to pickle file
+    if save_to_pickle:
+        # Check if path_dump is specified, if not so raise an exception
+        if path_dump is None:
+            raise Exception(
+                "Please specify the path that you wish to dump the result of this conversion."
+            )
+        # Then dump it
+        with (open(path_dump, "wb") as f):
+            pkl.dump(res, f)
 
-        ext_arr.append(ext)
+    return res
 
-    with_ext_df = pd.concat([df, pd.DataFrame({"ext": ext_arr})], axis=1)
 
-    if inplace:
-        df = with_ext_df
-
-    return with_ext_df
+def segment_cough(original_audio: np.ndarray, cough_mask: np.ndarray) -> np.ndarray:
+    ...
