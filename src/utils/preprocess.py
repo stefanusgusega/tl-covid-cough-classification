3ba863e92197203ab1f3887c30f3295c99b1c25c
stefanusgusega <gunawanstefanus006@gmail.com>
2022-03-01 14:03:52 +0700

    feat: add preprocessing procedure

---
+++ src/utils/preprocess.py
@@ -0,0 +1,34 @@
+from typing import Tuple
+import numpy as np
+import pandas as pd
+import pickle as pkl
+from utils.audio import convert_audio_to_numpy, generate_segmented_data
+
+
+def preprocess_dataframe(
+    df: pd.DataFrame,
+    audio_folder_path: str,
+    sampling_rate: int = 16000,
+    save_to_pickle=True,
+    path_dump=None,
+) -> Tuple[np.ndarray, np.ndarray]:
+    numpy_data, covid_statuses = convert_audio_to_numpy(
+        df, audio_folder_path=audio_folder_path, sampling_rate=sampling_rate
+    )
+
+    segmented_data, segmented_covid_status = generate_segmented_data(
+        numpy_data, covid_statuses, sampling_rate=sampling_rate
+    )
+
+    res = segmented_data, segmented_covid_status
+
+    # Save to pickle file
+    if save_to_pickle:
+        # Check if path_dump is specified, if not so raise an exception
+        if path_dump is None:
+            raise Exception(
+                "Please specify the path that you wish to dump the result of this conversion."
+            )
+        # Then dump it
+        with (open(path_dump, "wb")) as f:
+            pkl.dump(res, f)
