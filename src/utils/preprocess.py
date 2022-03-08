fef8fdda5efde7e4edca711e3d272c9c1aa6c741
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-08 10:27:29 +0700

    feat: add backup every stage param on preprocess

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,3 +1,4 @@
+import os
 from typing import Tuple
 import numpy as np
 import pandas as pd
@@ -19,18 +20,50 @@ def preprocess_dataframe(
     win_length: int = None,
     is_mel_spec: bool = True,
     save_to_pickle=True,
-    path_dump=None,
+    backup_every_stage=True,
+    pickle_folder=None,
 ) -> Tuple[np.ndarray, np.ndarray]:
+
+    if save_to_pickle or backup_every_stage:
+        # Check if the folder is specified, if not so raise an exception
+        if pickle_folder is None:
+            raise Exception(
+                "Please specify the path that you wish to dump the results of this conversion."
+            )
+
     numpy_data, covid_statuses = convert_audio_to_numpy(
         df, audio_folder_path=audio_folder_path, sampling_rate=sampling_rate
     )
+
+    if backup_every_stage:
+        print("Creating backup for numpy data...")
+        save_to_pickle(
+            (numpy_data, covid_statuses), os.path.join(pickle_folder, "numpy_data.pkl")
+        )
+        print("Backup for numpy data created.")
 
     segmented_data, segmented_covid_status = generate_segmented_data(
         numpy_data, covid_statuses, sampling_rate=sampling_rate
     )
 
+    if backup_every_stage:
+        print("Creating backup for segmented data...")
+        save_to_pickle(
+            (segmented_data, covid_statuses),
+            os.path.join(pickle_folder, "segmented_data.pkl"),
+        )
+        print("Backup for segmented data created.")
+
     # Pad the data
     padded_data = pad_audio_with_silence(segmented_data)
+
+    if backup_every_stage:
+        print("Creating backup for padded data...")
+        save_to_pickle(
+            (padded_data, covid_statuses),
+            os.path.join(pickle_folder, "padded_data.pkl"),
+        )
+        print("Backup for padded data created.")
 
     # Data augmentation
     balanced_data = padded_data
@@ -51,15 +84,15 @@ def preprocess_dataframe(
 
     # Save to pickle file
     if save_to_pickle:
-        # Check if path_dump is specified, if not so raise an exception
-        if path_dump is None:
-            raise Exception(
-                "Please specify the path that you wish to dump the result of this conversion."
-            )
-        # Then dump it
-        with (open(path_dump, "wb")) as f:
-            pkl.dump(res, f)
+        print("Saving features...")
+        save_to_pickle(res, os.path.join(pickle_folder, "features.pkl"))
+        print("Features saved.")
 
     # Returning series of data in (-1, 1) shape and the labels in (-1, 1) too
     # NOW : features in 2D shape
     return res
+
+
+def save_to_pickle(to_save, file_path):
+    with (open(file_path, "wb")) as f:
+        pkl.dump(to_save, f)
