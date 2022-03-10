c04722f697693c138fe78a66e3806376c0df4f38
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-10 21:50:57 +0700

    fix: make training to Trainer class

--- src/utils/training.py
+++ src/utils/training.py
@@ -1,24 +1,35 @@
 # TODO: implement sttratified k-fold validation
 import numpy as np
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 
 
-def stratified_k_fold_cross_validation(
-    audio_datas: np.ndarray, audio_labels: np.ndarray, n_splits: int = 5
-):
-    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+class Trainer:
+    def __init__(
+        self, audio_datas: np.ndarray, audio_labels: np.ndarray, test_size: float = 0.8
+    ) -> None:
+        self.X_full = audio_datas
 
-    # Label encode the audio labels
-    label_encoder = OrdinalEncoder(categories=[["healthy", "COVID-19"]])
-    labels_encoded = label_encoder.fit_transform(audio_labels)
+        print("Encoding labels...")
+        # Label encode the audio labels first
+        label_encoder = OrdinalEncoder(categories=[["healthy", "COVID-19"]])
+        self.y_full = label_encoder.fit_transform(audio_labels)
+        print("Labels encoded.")
 
-    for idx, (train_index, test_index) in enumerate(
-        skf.split(audio_datas, labels_encoded)
-    ):
-        ...
-        # TODO: do training
-        # 80 : 20 --> 80 will be stratified 5 fold cross validated
+        print("Splitting dataset to ready to train and test...")
+        # Split here in stratified fashion
+        self.X, self.X_test, self.y, self.y_test = train_test_split(
+            self.X_full, self.y_full, test_size=test_size, stratify=self.y
+        )
+        print("Dataset splitted.")
 
-        # X_train, X_test = audio_datas[train_index], audio_datas[test_index]
-        # y_train, y_test = audio_labels[train_index], audio_labels[test_index]
+    def stratified_k_fold_cross_validation(self, n_splits: int = 5):
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+        for idx, (train_index, val_index) in enumerate(skf.split(self.X, self.y)):
+            ...
+            # TODO: do training
+            # 80 : 20 --> 80 will be stratified 5 fold cross validated
+
+            # X_train, X_test = audio_datas[train_index], audio_datas[test_index]
+            # y_train, y_test = audio_labels[train_index], audio_labels[test_index]
