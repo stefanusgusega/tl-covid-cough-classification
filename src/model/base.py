c477b491531eedfb5fc0db54cc3834ebf19437ed
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-12 12:51:30 +0700

    fix: exception to when input shape is 3

--- src/model/base.py
+++ src/model/base.py
@@ -5,8 +5,8 @@ from typing import Tuple
 class BaseModel:
     def __init__(self, input_shape: Tuple, initial_weights=None) -> None:
         # Check whether the input shape of the model 2D
-        if len(input_shape) != 2:
-            raise Exception(f"The input shape should be 2D. Current : {input_shape}D.")
+        if len(input_shape) != 3:
+            raise Exception(f"The input shape should be 3D. Current : {input_shape}D.")
 
         self.input_shape = input_shape
         self.initial_weights = initial_weights
