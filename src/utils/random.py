39ee2e036ed90878da2fbe06ab06550e65825e2c
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-14 12:12:11 +0700

    feat: add random seeder and add todo on Trainer class

---
+++ src/utils/random.py
@@ -0,0 +1,18 @@
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+
+def set_random_seed(seed_value: int = 42):
+    # Set the PYTHONHASHSEED environment variable at a fixed value
+    os.environ["PYTHONHASHSEED"] = str(seed_value)
+
+    # Set the python built-in pseudorandom number generator
+    random.seed(seed_value)
+
+    # Set the numpy pseudorandom number generator
+    np.random.seed(seed_value)
+
+    # Set the tensorflow pseudorandom number generator
+    tf.random.set_seed(seed_value)
