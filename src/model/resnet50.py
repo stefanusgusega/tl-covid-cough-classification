b3957f9821ed74a27226fe25a8937316b51dccde
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-17 16:08:06 +0700

    feat: add mechanism to tune hyperparameter

--- src/model/resnet50.py
+++ src/model/resnet50.py
@@ -2,8 +2,20 @@ from typing import Tuple
 import tensorflow as tf
 from model.base import BaseModel
 
+HYPERPARAMETER_KEYS = [
+    "first_dense_units",
+    "second_dense_units",
+    "learning_rate",
+    "batch_size",
+    "epochs",
+]
+
 
 class ResNet50Model(BaseModel):
+    """
+    Main ResNet50 model for COVID-19 classification
+    """
+
     def __init__(
         self,
         input_shape: Tuple,
@@ -44,6 +56,51 @@ class ResNet50Model(BaseModel):
             optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
             loss=tf.keras.losses.BinaryCrossentropy(),
             metrics=metrics,
+        )
+
+        return self.model
+
+    def hyperparameter_tune_model(
+        self, first_dense_units, second_dense_units, learning_rate, epochs, batch_size
+    ):
+        """
+        Hyperparameters should contain these:
+        * first_dense_units
+        * second_dense_units
+        * learning_rate
+        * epochs
+        * batch_size
+        """
+        super().hyperparameter_tune_model()
+
+        # Check if hyperparameters dictionary is complete
+        # if list(hyperparameters.keys()) == HYPERPARAMETER_KEYS:
+        #     raise Exception(
+        #         f"The hyperparameters is not complete. Should state this keys: {set(HYPERPARAMETER_KEYS) - set(hyperparameters.keys())}"
+        #     )
+
+        # Build model
+        self.model = tf.keras.Sequential()
+
+        self.model.add(
+            tf.keras.application.resnet50.ResNet50(
+                include_top=self.include_resnet_top,
+                weights=self.initial_weights,
+                input_shape=self.input_shape,
+            )
+        )
+
+        self.model.add(tf.keras.layers.AveragePooling2D())
+        self.model.add(tf.keras.layers.Flatten())
+
+        self.model.add(tf.keras.layers.Dense(first_dense_units, activation="relu"))
+        self.model.add(tf.keras.layers.Dense(second_dense_units, activation="relu"))
+        self.model.add(tf.keras.layers.Dense(2, activation="softmax"))
+
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+            loss=tf.keras.losses.BinaryCrossentropy(),
+            metrics=[tf.keras.metrics.AUC()],
         )
 
         return self.model
