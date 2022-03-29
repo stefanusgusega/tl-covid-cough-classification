34c9c1bbf5075b0608dcb92728671e27828603d8
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-29 15:54:25 +0700

    feat: add identity block function

--- src/utils/model.py
+++ src/utils/model.py
@@ -57,3 +57,64 @@ def generate_tensorboard_callback(log_dir: str):
     specified_log_dir = os.path.join(log_dir, generate_now_datetime())
 
     return tf.keras.callbacks.TensorBoard(log_dir=specified_log_dir)
+
+
+def identity_block(input_tensor, middle_kernel_size, filters, stage, block):
+    """
+    Implementation of the identity block of ResNet
+
+    Arguments:
+    * input_tensor -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    * middle_kernel_size -- integer, specifying the shape of the middle CONV's window for the main path
+    * filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    * stage -- integer, used to name the layers, depending on their position in the network
+    * block -- string/character, used to name the layers, depending on their position in the network
+
+    Returns:
+    * model -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+
+    # Define name basis
+    conv_name_base = f"res{stage}{block}_branch"
+    bn_name_base = f"bn{stage}{block}_branch"
+
+    # First component of main path
+    model = tf.keras.layers.Conv2D(
+        filters=filters[0],
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding="valid",
+        name=f"{conv_name_base}2c",
+        kernel_initializer="glorot_uniform",
+    )(input_tensor)
+    model = tf.keras.layers.BatchNormalization(axis=3, name=f"{bn_name_base}2a")(model)
+    model = tf.keras.layers.Activation("relu")(model)
+
+    # Second component of main path
+    model = tf.keras.layers.Conv2D(
+        filters=filters[1],
+        kernel_size=(middle_kernel_size, middle_kernel_size),
+        strides=(1, 1),
+        padding="same",
+        name=f"{conv_name_base}2b",
+        kernel_initializer="glorot_uniform",
+    )(model)
+    model = tf.keras.layers.BatchNormalization(axis=3, name=f"{bn_name_base}2b")(model)
+    model = tf.keras.layers.Activation("relu")(model)
+
+    # Third component of main path
+    model = tf.keras.layers.Conv2D(
+        filters=filters[2],
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding="valid",
+        name=f"{conv_name_base}2c",
+        kernel_initializer="glorot_uniform",
+    )(model)
+    model = tf.keras.layers.BatchNormalization(axis=3, name=f"{bn_name_base}2c")(model)
+
+    # Final step: Add shortcut value, which is input tensor, to main path, and pass it through a RELU activation
+    model = tf.keras.layers.Add()([model, input_tensor])
+    model = tf.keras.layers.Activation("relu")(model)
+
+    return model
