a36a1bd404809f9c553fcd1a058102f872206bca
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-06-16 14:23:36 +0700

    feat: finish demo script

--- demo.py
+++ demo.py
@@ -4,11 +4,13 @@ Demonstration program
 import argparse
 import librosa
 import numpy as np
+import tensorflow as tf
 
 from src.utils.audio import generate_segmented_data
+from src.utils.preprocess import FeatureExtractor, encode_label, expand_mel_spec
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--mode", default="best_exp_feat")
+parser.add_argument("--mode", default="feat_ext")
 parser.add_argument("--audio")
 parser.add_argument("--label", default="COVID-19")
 args = parser.parse_args()
@@ -25,4 +27,34 @@ audio_segments, final_labels = generate_segmented_data(
     samples_data=audio_data.reshape((-1, 1)), audio_labels=np.array([[args.label]])
 )
 
-# TODO : add feature extractor class
+feature_extractor = FeatureExtractor(
+    backup_every_stage=False, offset=23380, for_training=False
+)
+X_test, y_test = feature_extractor.run(
+    aggregated_data=audio_segments,
+    aggregated_labels=final_labels,
+    n_mels=64,
+    hop_length=128,
+)
+
+# Encode the y_test
+y_test = encode_label(y_test, pos_label="COVID-19")
+
+# Expand dimension because ResNet expects 3D shape
+X_test = expand_mel_spec(X_test)
+
+# Load model based on what mode this demo is
+model = tf.keras.models.load_model(model_path_dict[args.mode])
+
+# Do prediction
+y_proba = model.predict(X_test)
+
+# Make label to 0 and 1
+y_pred = np.where(y_proba >= 0.5, 1, 0)
+
+print(f"There are {len(X_test)} segments of cough.")
+
+for idx, (pred_res, true_res) in enumerate(zip(y_pred, y_test)):
+    print(f"Result for segment no. {idx}")
+    print(f"True result: {true_res}")
+    print(f"Actual result: {pred_res}")
